322d751a1957e4727b68ed7142cb098073015b38
AlonsoFavero <alonso.favero@gmail.com>
2026-08-14 15:45:33 -0300

    Cria primeiro programa java

--- src/main.java
+++ src/main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+        System.out.println("meus primeiros passos para aprender java ");
+
+        String nome = "Alonso";
+        int idade = 18;
+
+        System.out.println(nome);
+        System.out.println(idade);
+    }
 }
