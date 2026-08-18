1dd05d9f94a97faa6362002bfadb0fa4b483a1e6
AlonsoFavero <alonso.favero@gmail.com>
2026-08-17 23:49:51 -0300

    Adiciona operação matemáticas

--- src/main.java
+++ src/main.java
@@ -7,5 +7,14 @@ public class main {
 
         System.out.println(nome);
         System.out.println(idade);
+        System.out.println("meu nome é " + nome + " e tenho " + idade + " anos. ");
+
+        int numero1 = 10;
+        int numero2 = 5;
+
+        System.out.println(numero1 + numero2);
+        System.out.println(numero1 - numero2);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero1 / numero2);
     }
 }
