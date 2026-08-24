eebd1e763205122b26087a890797d5155297616a
AlonsoFavero <alonso.favero@gmail.com>
2026-08-24 01:20:04 -0300

    Adiciona toString em coleções de Pessoa

--- src/TesteToString.java
+++ src/TesteToString.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class TesteToString {
 
     public static void main(String[] args) {
 
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
+        HashSet<Pessoa> pessoasUnicas = new HashSet<>();
+
         Pessoa pessoa = new Pessoa("Alonso", 17);
-        System.out.println(pessoa);
+        Pessoa pessoa1 = new Pessoa("Carlos", 25);
+
+        pessoasUnicas.add(pessoa);
+        pessoasUnicas.add(pessoa1);
+        pessoas.add(pessoa);
+        pessoas.add(pessoa1);
+
+        System.out.println(pessoasUnicas);
+        System.out.println(pessoas);
     }
 }
