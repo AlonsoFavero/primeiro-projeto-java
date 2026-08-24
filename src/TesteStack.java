209c6a035bf274d54809ff7f86bac45113be9c32
AlonsoFavero <alonso.favero@gmail.com>
2026-08-24 13:23:50 -0300

    Pratica estrutura Stack

---
+++ src/TesteStack.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class TesteStack {
+    public static void main(String[] args){
+
+        Stack<String> pilha = new Stack<>();
+
+        pilha.push("Alonso");
+        pilha.push("Carlos");
+        pilha.push("João");
+
+        System.out.println(pilha);
+
+        System.out.println(pilha.pop());
+
+        System.out.println(pilha);
+
+        System.out.println(pilha.peek());
+
+        System.out.println(pilha.isEmpty());
+
+        System.out.println(pilha.pop());
+        System.out.println(pilha.pop());
+
+        System.out.println(pilha);
+        System.out.println(pilha.isEmpty());
+
+    }
+}
