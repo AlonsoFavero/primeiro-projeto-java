033b6a5e51da3a9e1bcfafa4da46bc58032cb22c
AlonsoFavero <alonso.favero@gmail.com>
2026-08-24 01:37:32 -0300

    Pratica equals e hashCode com HashSet

---
+++ src/TesteEquals.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class TesteEquals {
+    public static void main(String[] args){
+
+        HashSet<Pessoa> pessoaHashSet = new HashSet<>();
+
+        Pessoa pessoa1 = new Pessoa("Alonso", 16);
+        Pessoa pessoa2 = new Pessoa("Alonso", 16);
+        Pessoa pessoa3 = new Pessoa("Carlos", 16);
+
+        System.out.println(pessoa1.equals(pessoa2));
+        System.out.println(pessoa1.equals(pessoa3));
+
+        System.out.println(pessoa1.hashCode());
+        System.out.println(pessoa2.hashCode());
+        System.out.println(pessoa3.hashCode());
+
+        pessoaHashSet.add(pessoa1);
+        pessoaHashSet.add(pessoa2);
+        pessoaHashSet.add(pessoa3);
+
+        System.out.println(pessoaHashSet.size());
+    }
+}
