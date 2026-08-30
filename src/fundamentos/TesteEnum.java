ae67f95bb5673d6752c864243b236f853e3f91f7
AlonsoFavero <alonso.favero@gmail.com>
2026-08-30 18:31:44 -0300

    Adiciona estudo de Enum

---
+++ src/fundamentos/TesteEnum.java
@@ -0,0 +1,34 @@
+package fundamentos;
+
+public class TesteEnum {
+
+    public static void main(String[] args) {
+
+        DiaSemana dia = DiaSemana.SEGUNDA;
+
+        System.out.println(dia);
+
+        for (DiaSemana d : DiaSemana.values()) {
+
+            System.out.println(d.ordinal());
+
+        }
+
+        DiaSemana diaEscolhido = DiaSemana.valueOf("SEXTA");
+
+        System.out.println(diaEscolhido);
+    }
+
+}
+
+enum DiaSemana {
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO,
+    DOMINGO
+
+}
+
