3debc1f8cd5b5cfb4b7940886366e80b6980c4e9
AlonsoFavero <alonso.favero@gmail.com>
2026-08-31 22:34:24 -0300

    Adiciona estudo de throws

---
+++ src/fundamentos/TesteThrows.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class TesteThrows {
+    public static void main(String[] args){
+
+        try{
+            verificarIdade(16);
+        }catch (IllegalArgumentException e){
+
+            System.out.println("error vc é menor de idade");
+
+        }
+
+    }
+    public static void verificarIdade(int idade) throws IllegalArgumentException{
+
+        if(idade < 18){
+            throw new IllegalArgumentException();
+        }
+    }
+}
